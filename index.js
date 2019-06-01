9374955477bd48127b1ac32508095eb09fbe61bd
mjk287 <myung.kim287@gmail.com>
2019-06-01 19:33:32 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 function findMinAndRemove(array){
+  let min = array[0]
+  let minIndex = 0
 
+  for (let i = 0; i < array.length; i ++ ){
+    if(array[i] < min){
+      min = array[i]
+      minIndex = i
+    }
+  }
+  array.splice(minIndex, 1)
+  return min
 }
 
 function insertionSort(array){
+  // create new sorted array
+  let newArray = []
+  // find the minimum value in array and append it to new array
+  while(array.length > 0){
+    newArray.push(findMinAndRemove(array))
+  }
 
+  return newArray
 }
